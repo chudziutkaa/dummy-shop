2efdc5fb40f80550f8c3a8d2c41cd7171dadb423
chudziutkaa <klaudiachudy94@gmail.com>
2017-05-06 19:59:42 +0200

    Change redirect paths after admin signing in & signing out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include ApplicationHelper
+
+  def after_sign_in_path_for(resource)
+    admin_root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_user_session_path
+  end
+
 end
