cc1f9b0a844d982e50b1627ede9bd08a3e4c7c4d
chudziutkaa <klaudiachudy94@gmail.com>
2017-05-06 20:07:02 +0200

    Change Product.price validation
    to prevent negative price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   validates :name, presence: true
   validates :description, length: { maximum: 300 }
+  validates :price, numericality: { greater_than: 0 }, presence: true
 
   has_many :order_products
 
