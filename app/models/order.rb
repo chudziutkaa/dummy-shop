fd5a58c35fdd1088984885feeb7dcaed45b54c36
chudziutkaa <klaudiachudy94@gmail.com>
2017-05-07 15:48:39 +0200

    Change Order.order_status enum
    symbols into strings

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,10 +3,12 @@ class Order < ActiveRecord::Base
   has_one :user
   accepts_nested_attributes_for :user, update_only: true
 
-  enum order_status: %i(:completed, :cart)
+  enum order_status: %i(completed cart)
 
   def total_price
     order_products.collect(&:price).sum
   end
 
+  # scope :categorized, ->(names) { where order_status: names.map { |name| Order.order_statuses[name]} }
+  scope :categorized, ->(name){ where order_status: name }
 end
