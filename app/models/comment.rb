00f550761ad4dbc90e28b088c00258f32515ddb2
chudziutkaa <klaudiachudy94@gmail.com>
2017-05-08 00:35:11 +0200

    Change validation for Comment
    if input is blank comment won't create

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ActiveRecord::Base
-  belongs_to :product
+  belongs_to :product, required: true
   belongs_to :parent, class_name: 'Comment'
   has_many :children, class_name: 'Comment', foreign_key: 'parent_id'
 
-  validates :content, length: { maximum: 150 }
+  validates :content, length: { in: 1..150 }
 
   scope :from_recent, ->{ order('created_at DESC') }
 end
